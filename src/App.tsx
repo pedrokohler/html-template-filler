2328bbf4ad7da65b2ae893c201e702dada50144c
pedrokohler <pedrokohlerbh@gmail.com>
2022-02-12 14:52:38 -0300

    Refator: add accordion

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import { html as beautifyHtml } from "js-beautify";
 import {
   Button,
+  Col,
   Container,
   FormControl,
-  InputGroup,
   Row,
+  Accordion,
 } from "react-bootstrap";
 
 import { SimpleTemplate } from "./templates";
 import { IDownloadSection, IFeaturesList } from "./interfaces";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
 
 const downloadSections: IDownloadSection[] = [
   {
@@ -55,14 +57,32 @@ const featuresList: IFeaturesList[] = [
       "(1) O-RING",
     ],
   },
+  {
+    title: "Compatível Com:",
+    properties: [
+      "BOMBAS CENTRÍFUGAS HYPRO DAS SÉRIES:",
+      "9200",
+      "9300, INCLUINDO 9303 E 9306",
+      "9304",
+    ],
+  },
+  {
+    title: "Características:",
+    properties: [
+      "VIDA ÚTIL 8 VEZES SUPERIOR SOB PRODUTOS ABRASIVOS.",
+      "DUAS FACES EM CARBETO DE SILÍCIO.",
+      "REDUÇÃO DA NECESSIDADE DE REPOSIÇÃO.",
+      "UPGRADE DISPONÍVEL PARA BOMBAS DE FERRO FUNDIDO",
+    ],
+  },
 ];
 
 function App() {
-  const Element = () => (
+  const FilledTemplate = () => (
     <>
       <SimpleTemplate
         downloadSections={downloadSections}
-        mainParagraphText=" HYPRO é uma marca americana líder no setor que fabrica bombas desde 1947, qualidade testada e comprovada. As bombas centrífugas HYPRO são utilizadas pela maioria dos fabricantes de máquinas agrícolas no Brasil e no mundo."
+        mainParagraphText="HYPRO é uma marca americana líder no setor que fabrica bombas desde 1947, qualidade testada e comprovada. As bombas centrífugas HYPRO são utilizadas pela maioria dos fabricantes de máquinas agrícolas no Brasil e no mundo."
         secondaryParagraphText="A dureza do Carbeto de Silício se aproxima da do diamante e permite que a vedação tenha uma vida útil de serviço excelente ao bombear soluções abrasivas. Atonge até 8x mais durabilidade quando comparado com o reparo em cerâmica. Os flocos de grafite de carbono são incorporados nas faces de carbeto de silício para reduzir o atrito e proporcionar capacidade de sobrevivência a choque térmico e operação a seco acidentais."
         footerTitle="QUER GARANTIR A VIDA ÚTIL DE SUA BOMBA HYPRO?"
         footerParagraphTexts={footerParagraphTexts}
@@ -70,23 +90,38 @@ function App() {
       ></SimpleTemplate>
     </>
   );
-  const pretty = beautifyHtml(renderToString(<Element></Element>));
+  const prettifiedTemplateHtml = beautifyHtml(renderToString(<FilledTemplate></FilledTemplate>));
 
   return (
     <Container>
+      <Row></Row>
       <Row>
-        <InputGroup>
-          <InputGroup.Text>HTML</InputGroup.Text>
-          <FormControl as="textarea" aria-label="With textarea" value={pretty}/>
-        </InputGroup>
+        <Col>
+          <CopyToClipboard text={prettifiedTemplateHtml}>
+            <Button>Copy to clipboard</Button>
+          </CopyToClipboard>
+        </Col>
       </Row>
       <Row>
-        <CopyToClipboard text={pretty}>
-          <Button>Copy to clipboard</Button>
-        </CopyToClipboard>
-      </Row>
-      <Row>
-        <Element></Element>
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Ver HTML</Accordion.Header>
+            <Accordion.Body>
+              <FormControl
+                className="html-preview"
+                as="textarea"
+                aria-label="With textarea"
+                value={prettifiedTemplateHtml}
+              />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Pré-Visualização</Accordion.Header>
+            <Accordion.Body>
+              <FilledTemplate></FilledTemplate>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
       </Row>
     </Container>
   );
